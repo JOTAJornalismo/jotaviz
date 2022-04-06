27b40b1effd91da6420e93aa5bebb84f1bce88cc
daniel <dmarcelinobr@live.com>
2022-04-06 10:02:47 -0300

    ajuste nos ticks

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ class PostInstallation(install):
 
 setup(
     name='jotaviz',
-    version='1.0.4',
+    version='1.0.5',
     author='Daniel Marcelino',
     author_email='daniel.marcelino@jota.info',
     description='Matplotlib jotaviz style format for publications at jota.info',
