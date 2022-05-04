d0c93d14f65896f8bb3309a30aad89194df49b67
daniel <dmarcelinobr@live.com>
2022-05-04 19:46:30 -0300

    viz

--- jotaviz/__init__.py
+++ jotaviz/__init__.py
@@ -1,5 +1,5 @@
 """jotaviz - A collection of plotting functions and styles used at jota.info"""
 
-__version__ = "1.0.3"
+__version__ = "1.0.8"
 __author__ = 'Daniel Marcelino <daniel.marcelino@jota.info>'
 from jotaviz.utils import *
